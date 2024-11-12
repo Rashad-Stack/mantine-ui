8f1a45ff89f9de2839344a3b7d6fcb9999151186
Rashad-Stack <tex.eng.rashed@gmail.com>
2024-11-12 19:18:22 +0600

    feat: integrate loading state in Home and RootLayout components; display loading indicator during data fetching

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,19 @@
 import { useEffect } from "react";
 import { useLoaderData } from "react-router-dom";
-import Loading from "../components/Loading";
 import Products from "../components/Products";
 import useGlobalState from "../hooks/useGlobalState";
 
 export default function Home() {
   const books = useLoaderData();
-  const { dispatch, isLoading } = useGlobalState();
+  const { dispatch } = useGlobalState();
 
   useEffect(() => {
     dispatch({ type: "SET_BOOKS", payload: books });
   }, [books, dispatch]);
 
-  return <>{isLoading ? <Loading /> : <Products />}</>;
+  return (
+    <>
+      <Products />
+    </>
+  );
 }
--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,12 +1,14 @@
 import { useEffect } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Loading from "../components/Loading";
 import { auth } from "../firebase/config";
 import useGlobalState from "../hooks/useGlobalState";
 
 export default function RootLayout() {
-  const { dispatch } = useGlobalState();
+  const { dispatch, isLoading } = useGlobalState();
+  const { state } = useNavigation();
 
   useEffect(() => {
     if (auth.currentUser) {
@@ -18,6 +20,7 @@ export default function RootLayout() {
 
   return (
     <div className="flex min-h-screen flex-col">
+      {(isLoading || state === "loading") && <Loading />}
       <Header />
       <main className="flex-1 p-4">
         {/* This is where the page content will go */}
